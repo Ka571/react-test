e56fd5ce11549c3c669aeecd980369947c7328ec
Ka571 <842335772@qq.com>
2019-08-19 15:41:54 +0800

    build

---
+++ precache-manifest.88448cb1d89e1c4fa39510709271e8b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7b07ace11bbc1da303aa8a53c4498b",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "5399bc6759894ddc87cc",
+    "url": "/react-test/static/css/main.c13f90a9.chunk.css"
+  },
+  {
+    "revision": "a48fea2cf8e56dd5c575",
+    "url": "/react-test/static/js/2.54240292.chunk.js"
+  },
+  {
+    "revision": "5399bc6759894ddc87cc",
+    "url": "/react-test/static/js/main.72f07ca0.chunk.js"
+  },
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  }
+]);
